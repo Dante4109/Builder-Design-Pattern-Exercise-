8e5d1c8b399d9297bdce16af146ed9acba0e14f3
RJ Zeller <rogerjohnmorellizeller@gmail.com>
2018-07-21 14:38:59 -0400

    start of code

--- Builder Design Pattern Practice/Program.cs
+++ Builder Design Pattern Practice/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace Builder_Design_Pattern_Practice
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            //var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            //Console.WriteLintgfe(cb);
+
         }
     }
+
+    
+    
 }
+
+
+
